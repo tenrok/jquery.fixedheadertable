57cfdde7817a510934247afa084b4e1ff1c3b593
Feng Xue <feng.xue@nsd.uib.no>
2013-07-12 15:12:45 +0200

    Add functionality of fixed columns with multiple rows, check out the examples in the test.html Also changed a little to defaultTheme.css Use jslint to format the codes

--- demo/demo.js
+++ demo/demo.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
     $('#myTable01').fixedHeaderTable({ footer: true, cloneHeadToFoot: true, altClass: 'odd', autoShow: false });
-    
+
     $('#myTable01').fixedHeaderTable('show', 1000);
-    
+
     $('#myTable02').fixedHeaderTable({ footer: true, altClass: 'odd' });
-    
+
     $('#myTable05').fixedHeaderTable({ altClass: 'odd', footer: true, fixedColumns: 1 });
-    
+
     $('#myTable03').fixedHeaderTable({ altClass: 'odd', footer: true, fixedColumns: 1 });
-    
-    $('#myTable04').fixedHeaderTable({ altClass: 'odd', footer: true, cloneHeadToFoot: true, fixedColumns: 3 });
+
+    $('#myTable04').fixedHeaderTable({ altClass: 'odd', footer: true, cloneHeadToFoot: true, fixedColumns: 3});
+
+    $('#myTable06').fixedHeaderTable({ altClass: 'odd', footer: true, cloneHeadToFoot: true, fixedColumns: 1});
+
+    $('#myTable07').fixedHeaderTable({ altClass: 'odd', footer: true, cloneHeadToFoot: true, fixedColumns: 2});
 });
